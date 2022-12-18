1fe5ada6a9bfeec22a415c2293d9244ddd9a06e9
Ashish Sharda <ashishjsharda@gmail.com>
2022-12-18 06:44:36 -0600

    Add recordreader

--- src/main/java/IrisClassification.java
+++ src/main/java/IrisClassification.java
@@ -1,5 +1,17 @@
+
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.split.FileSplit;
+import org.nd4j.linalg.io.ClassPathResource;
 public class IrisClassification {
     public static void main(String[] args) {
+
+        RecordReader recordReader = new CSVRecordReader(0, ',');
+        try {
+            recordReader.initialize(new FileSplit(new ClassPathResource("iris.csv").getFile()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
